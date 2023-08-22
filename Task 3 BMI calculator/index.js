74d74c3c0234cd4efe0c53413834721a785ea0ed
milpen <milpen@mail.ru>
2023-08-22 22:04:17 +0500

    task 3 in progress

---
+++ Task 3 BMI calculator/index.js
@@ -0,0 +1,27 @@
+const weightInput = document.getElementById('weight');
+const heightInput = document.getElementById('height');
+const button = document.getElementById('button');
+const result = document.getElementById('BMI');
+
+
+function calculateBMI(event) {
+event.preventDefault();
+const weight =+weightInput.value;
+const height = +heightInput.value / 100;
+const doubledHeight = Math.pow(height, 2);
+const BMI = weight / doubledHeight;
+const finalBMI = BMI.toFixed(2);
+result.innerHTML = finalBMI;
+const p = document.createElement('p');
+
+if (result.innerHTML < 18.5) {
+    p.textContent = "Недостаточный вес";
+} else if(result.innerHTML >= 18.5 && result.innerHTML < 24.9) {
+    p.textContent = "Нормальный вес";
+} else if(result.innerHTML >=24.9 && result.innerHTML < 29.9) {
+    p.textContent = "Избыточный вес";
+}
+result.appendChild(p);
+}
+
+button.addEventListener('click', calculateBMI);
