3f0b69f07c37079af65e492e5eebec35f2d01ad5
milpen <milpen@mail.ru>
2023-08-24 22:04:19 +0500

    calculator done

--- Task 3 BMI calculator/index.js
+++ Task 3 BMI calculator/index.js
@@ -20,6 +20,12 @@ if (result.innerHTML < 18.5) {
     p.textContent = "Нормальный вес";
 } else if(result.innerHTML >=24.9 && result.innerHTML < 29.9) {
     p.textContent = "Избыточный вес";
+} else if(result.innerHTML >=30.0 && result.innerHTML < 34.9) {
+    p.textContent = "Ожирение 1 степени";
+} else if(result.innerHTML >=35.0 && result.innerHTML < 39.9) {
+    p.textContent = "Ожирение 2 степени";
+} else if(result.innerHTML >=40.0) {
+    p.textContent = "Тяжелое ожирение, срочно вызовите скорую!";
 }
 p.classList.add('text_result');
 result.append(p);
